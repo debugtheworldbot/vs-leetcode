4f7b1286696c7cf81d9420ecbb2dd50604e50439
田成壮 <istiancz@gmail.com>
2020-11-09 17:04:54 +0800

    first try

---
+++ 7.整数反转.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=javascript
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    let result = 0
+    while (x !== 0) {
+        result = result * 10 + x % 10;
+        x = (x / 10) | 0;
+    }
+    return (result | 0) === result ? result : 0;
+};
+// @lc code=end
+
