71b5cae5f842d6dd9bfe9d4aa5a6faecbd67bcfe
tian <istiancz@gmail.com>
2021-06-09 15:57:25 +0800

    binary

---
+++ 102.二叉树的层序遍历.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  const result = [];
+  if (!root) {
+    return result;
+  }
+  const q = [root];
+  while (q.length !== 0) {
+    const currentSize = q.length;
+    result.push([]);
+    for (let i = 1; i <= currentSize; i++) {
+      const node = q.shift();
+      result[result.length - 1].push(node.val);
+      if (node.left) q.push(node.left);
+      if (node.right) q.push(node.right);
+    }
+  }
+  return result;
+};
+// @lc code=end
